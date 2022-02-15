19c35ed2f11782cc259b4cea9db5b5be1e3ff419
changhyun.kim <changhyun.kim@ssvt.co.kr>
2022-02-15 11:23:01 +0900

    Add typescript settings

---
+++ typescript.js
@@ -0,0 +1,11 @@
+module.exports = {
+    parser: "@typescript-eslint/parser",
+    plugins: ["@typescript-eslint"],
+    rules: {
+        "@typescript-eslint/explicit-function-return-type": "off",
+        "@typescript-eslint/explicit-module-boundary-types": "off",
+        "@typescript-eslint/no-empty-function": "off",
+        "@typescript-eslint/ban-types": "off",
+        "@typescript-eslint/ban-ts-comment": "off",
+    },
+};
